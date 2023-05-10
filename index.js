a4db4174403b1a896572c541d3ba095ed78b371c
ray837 <59028643+ray837@users.noreply.github.com>
2023-05-10 14:37:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,47 @@
 const express = require('express');
 const app = express();
 
-// In-memory data store to keep track of the state of each transaction
+ 
 const transactions = {};
 
-// Route to handle the POST request
+ 
 app.post('/transactions/:id', (req, res) => {
-  const transactionId = req.params.id; // Get the transaction ID from the URL parameter
+    const transactionId = req.params.id;
+const stat=req.params.con;
 
-  // Check if the transaction ID is valid
-  if (isValidTransactionId(transactionId)) {
-    // Store the state of the transaction in the in-memory data store
-    transactions[transactionId] = { status: 'pending' };
+    if (true) {
 
-    // Send a response to indicate that the transaction has been received
-    res.status(200).send('Transaction received successfully.');
-  } else {
-    // Return an error response if the transaction ID is invalid
-    res.status(400).send('Invalid transaction ID.');
-  }
+        transactions[transactionId] = { status: "success"};
+
+
+        res.status(200).send('Transaction received successfully.');
+    } else {
+
+        res.status(400).send('Invalid transaction ID.');
+    }
 });
 
-// Route to handle the confirmation request from app B
-app.post('/transactions/:id/confirm', (req, res) => {
-  const transactionId = req.params.id; // Get the transaction ID from the URL parameter
 
-  // Check if the transaction ID is valid
-  if (isValidTransactionId(transactionId)) {
-    // Update the state of the transaction in the in-memory data store
-    transactions[transactionId].status = 'success';
+app.get('/transactions/:id', (req, res) => {
+    const transactionId = req.params.id;
 
-    // Send a response to indicate that the confirmation has been received
-    res.status(200).send('Confirmation received successfully.');
-  } else {
-    // Return an error response if the transaction ID is invalid
-    res.status(400).send('Invalid transaction ID.');
-  }
+
+    if (transactions[transactionId]) {
+        if (transactions[transactionId].status == 'success') {
+
+            res.status(200).send(transactions[transactionId].status);
+        } else {
+
+            res.status(400).send('Invalid transaction ID.');
+        }
+
+    } else {
+        res.status(200).send("pending");
+    }
 });
 
-// Function to check if the transaction ID is valid
-function isValidTransactionId(transactionId) {
-  // Perform validation logic here (e.g. check if the transaction ID is in the correct format)
-  // Return true if the transaction ID is valid, false otherwise
-  return true;
-}
 
-// Start the server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+    console.log(`Server running on port ${port}.`);
 });
